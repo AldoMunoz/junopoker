06733fe814068ea93c5cd476968aed331a5476b4
AldoMunoz <aldothenal@gmail.com>
2023-07-18 21:45:21 -0400

    Initial commit

---
+++ junopoker/src/main/java/com/v1/junopoker/JunopokerApplication.java
@@ -0,0 +1,13 @@
+package com.v1.junopoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunopokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunopokerApplication.class, args);
+	}
+
+}
---
+++ junopoker/src/test/java/com/v1/junopoker/JunopokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.junopoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunopokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

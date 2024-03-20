594ede561c79d9e351501796616d7515816797e0
AldoMunoz <aldothenal@gmail.com>
2024-03-20 18:12:07 -0400

    removed "remove-player.js", moved function to "add-player.js"

--- junopoker/src/main/resources/static/js/add-player.js
+++ junopoker/src/main/resources/static/js/add-player.js
@@ -140,6 +140,24 @@ async function sitPlayerEvent(message) {
 
     stompClient.send("/app/countPlayers", {}, $("#table-id").val());
 }
+
+function removePlayer() {
+    const settingsBar = $('.settings-bar');
+    const seat = settingsBar.attr('data-seat');
+    console.log("Seat value in removePlayer()", seat);
+
+    const playerRequest = {
+        type: "STAND",
+        player: null,
+        seatIndex: seat,
+        tableID: $("#table-id").val()
+    };
+
+    stompClient.send("/app/removePlayer", {}, JSON.stringify(playerRequest));
+    isSeated = false;
+
+}
+
 async function standPlayerEvent(message) {
     console.log("Stand ", message);
 
